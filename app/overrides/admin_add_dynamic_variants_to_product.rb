ded58ddb6f47cbeb9978e4bad626cd83043d3bed
tyler <84726170+embold-tyler@users.noreply.github.com>
2025-08-04 16:21:20 -0700

    :bug: Fix zeitwork errors

--- app/overrides/admin_add_dynamic_variants_to_product.rb
+++ app/overrides/admin_add_dynamic_variants_to_product.rb
@@ -1,13 +1,17 @@
-Deface::Override.new(
-  name: 'admin_product_dynamic_variants',
-  virtual_path: 'spree/admin/products/_form',
-  insert_after: '[data-hook="admin_product_form_promotionable"]',
-  text:
-     '<div data-hook="admin_product_form_dynamic_variants">
-        <%= f.field_container :dynamic_variants do %>
-          <%= f.label :dynamic_variants do %>
-            <%= f.check_box :dynamic_variants %> <%= I18n.t("spree.dynamic_variants") %>
+# frozen_string_literal: true
+
+module AdminAddDynamicVariantsToProduct
+  Deface::Override.new(
+    name: 'admin_product_dynamic_variants',
+    virtual_path: 'spree/admin/products/_form',
+    insert_after: '[data-hook="admin_product_form_promotionable"]',
+    text:
+        '<div data-hook="admin_product_form_dynamic_variants">
+          <%= f.field_container :dynamic_variants do %>
+            <%= f.label :dynamic_variants do %>
+              <%= f.check_box :dynamic_variants %> <%= I18n.t("spree.dynamic_variants") %>
+            <% end %>
           <% end %>
-        <% end %>
-      </div>'
-)
+        </div>'
+  )
+end
--- app/overrides/admin_add_surcharge_to_option_value.rb
+++ app/overrides/admin_add_surcharge_to_option_value.rb
@@ -1,16 +1,20 @@
-Deface::Override.new(
-  name: 'admin_option_value_surcharge_header',
-  virtual_path: 'spree/admin/option_types/edit',
-  insert_before: '[data-hook="option_header"] tr th:last-child',
-  text: '<th><%= I18n.t("spree.surcharge") %></th>'
-)
+# frozen_string_literal: true
 
-Deface::Override.new(
-  name: 'admin_option_value_surcharge_value',
-  virtual_path: 'spree/admin/option_types/_option_value_fields',
-  insert_before: '[data-hook="option_value"] td:last-child',
-  text:
-    '<td class="surcharge">
-       <%= f.text_field :surcharge, :value => number_to_currency(f.object.surcharge, :unit => "") %>
-     </td>'
-)
+module AdminAddSurchargeToOptionValue
+  Deface::Override.new(
+    name: 'admin_option_value_surcharge_header',
+    virtual_path: 'spree/admin/option_types/edit',
+    insert_before: '[data-hook="option_header"] tr th:last-child',
+    text: '<th><%= I18n.t("spree.surcharge") %></th>'
+  )
+
+  Deface::Override.new(
+    name: 'admin_option_value_surcharge_value',
+    virtual_path: 'spree/admin/option_types/_option_value_fields',
+    insert_before: '[data-hook="option_value"] td:last-child',
+    text:
+      '<td class="surcharge">
+        <%= f.text_field :surcharge, :value => number_to_currency(f.object.surcharge, :unit => "") %>
+      </td>'
+  )
+end
